e1a2face372c1a0a431f8e5cf0b3dcf5846a85c3
Kishore Mongar <kishore.mongar@aptagrim.com>
2024-05-15 18:42:15 +0530

    refactor(login): token creation fixes in progress

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,38 +1,47 @@
-const CustomError = require('../errors');
-const { isTokenValid } = require('../utils');
-const Token = require('../models/Token');
-const { attachCookiesToResponse } = require('../utils');
+const CustomError = require("../errors");
+const { isTokenValid } = require("../utils");
+const Token = require("../models/Token");
+const { attachCookiesToResponse } = require("../utils");
 
 const authenticateUser = async (req, res, next) => {
   const { refreshToken, accessToken } = req.signedCookies;
-
+  const authHeader = req.headers.authorization;
+  console.log("headers", req.user, isTokenValid(authHeader.split(" ")[1]));
   try {
     if (accessToken) {
       const payload = isTokenValid(accessToken);
       req.user = payload.user;
       return next();
+    } else if (refreshToken) {
+      const payload = isTokenValid(refreshToken);
+
+      const existingToken = await Token.findOne({
+        user: payload.user.userId,
+        refreshToken: payload.refreshToken,
+      });
+
+      if (!existingToken || !existingToken?.isValid) {
+        throw new CustomError.UnauthenticatedError("Authentication Invalid");
+      }
+
+      attachCookiesToResponse({
+        res,
+        user: payload.user,
+        refreshToken: existingToken.refreshToken,
+      });
+
+      req.user = payload.user;
+      return next();
+    } else if (authHeader?.startsWith("Bearer")) {
+      const token = authHeader.split(" ")[1];
+      const payload = isTokenValid(token);
+      req.user = payload.user;
+      return next();
+    } else {
+      throw new CustomError.UnauthenticatedError("Authentication Invalid");
     }
-    const payload = isTokenValid(refreshToken);
-
-    const existingToken = await Token.findOne({
-      user: payload.user.userId,
-      refreshToken: payload.refreshToken,
-    });
-
-    if (!existingToken || !existingToken?.isValid) {
-      throw new CustomError.UnauthenticatedError('Authentication Invalid');
-    }
-
-    attachCookiesToResponse({
-      res,
-      user: payload.user,
-      refreshToken: existingToken.refreshToken,
-    });
-
-    req.user = payload.user;
-    next();
   } catch (error) {
-    throw new CustomError.UnauthenticatedError('Authentication Invalid');
+    throw new CustomError.UnauthenticatedError("Authentication Invalid");
   }
 };
 
@@ -40,7 +49,7 @@ const authorizePermissions = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
       throw new CustomError.UnauthorizedError(
-        'Unauthorized to access this route'
+        "Unauthorized to access this route"
       );
     }
     next();
