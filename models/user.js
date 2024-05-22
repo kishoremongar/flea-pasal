267df4cc85136430a96433ec8cd4548e8c176453
Kishore Mongar <84139548+fushigina-Shinobi@users.noreply.github.com>
2024-05-23 01:39:37 +0530

    refactor(user): schema updated for profile edit

--- models/user.js
+++ models/user.js
@@ -1,32 +1,32 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Please provide name'],
+    required: [true, "Please provide name"],
     minlength: 3,
     maxlength: 50,
   },
   email: {
     type: String,
     unique: true,
-    required: [true, 'Please provide email'],
+    required: [true, "Please provide email"],
     validate: {
       validator: validator.isEmail,
-      message: 'Please provide valid email',
+      message: "Please provide valid email",
     },
   },
   password: {
     type: String,
-    required: [true, 'Please provide password'],
+    required: [true, "Please provide password"],
     minlength: 6,
   },
   role: {
     type: String,
-    enum: ['admin', 'user'],
-    default: 'user',
+    enum: ["admin", "user"],
+    default: "user",
   },
   verificationToken: String,
   isVerified: {
@@ -40,12 +40,74 @@ const UserSchema = new mongoose.Schema({
   passwordTokenExpirationDate: {
     type: Date,
   },
+  profilePicture: {
+    type: String,
+  },
+  birthday: {
+    type: String,
+  },
+  gender: {
+    type: String,
+  },
+  addresses: [
+    {
+      street: {
+        type: String,
+        required: true,
+      },
+      apartmentNo: {
+        type: String,
+      },
+      pincode: {
+        type: String,
+        required: true,
+      },
+      landmark: {
+        type: String,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      state: {
+        type: String,
+        required: true,
+      },
+      country: {
+        type: String,
+        required: true,
+      },
+      isPrimary: {
+        type: Boolean,
+        default: false,
+      },
+    },
+  ],
+  socialLinks: {
+    facebook: {
+      type: String,
+      maxlength: 100,
+    },
+    twitter: {
+      type: String,
+      maxlength: 100,
+    },
+    instagram: {
+      type: String,
+      maxlength: 100,
+    },
+  },
 });
 
-UserSchema.pre('save', async function () {
+UserSchema.path("addresses").validate(function (addresses) {
+  const primaryAddresses = addresses.filter((address) => address.isPrimary);
+  return primaryAddresses.length <= 1;
+}, "Only one address can be marked as primary");
+
+UserSchema.pre("save", async function () {
   // console.log(this.modifiedPaths());
   // console.log(this.isModified('name'));
-  if (!this.isModified('password')) return;
+  if (!this.isModified("password")) return;
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
@@ -55,4 +117,4 @@ UserSchema.methods.comparePassword = async function (canditatePassword) {
   return isMatch;
 };
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
