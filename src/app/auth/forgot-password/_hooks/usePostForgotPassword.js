01e6ef6c88ecbd4a3976101698de48506e62ddbe
Kishore Mongar <kishore.mongar@aptagrim.com>
2024-05-27 17:01:03 +0530

    feat: change and forgot password added

--- src/app/auth/forgot-password/_hooks/usePostForgotPassword.js
+++ src/app/auth/forgot-password/_hooks/usePostForgotPassword.js
@@ -1,26 +1,26 @@
-import { useRouter } from 'next/navigation';
 import { useMutation } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
 import apiEndPoints from '@/services/apiEndPoints';
 import makeApiRequest from '@/services/makeApiRequest';
 import { ErrorToast, SuccessToast } from '@/services/toasterServices';
 
-export default function usePostResetPassword(cb) {
+export default function usePostForgotPassword(cb) {
   const router = useRouter();
   return useMutation({
     mutationFn: async (data) => {
       const response = await makeApiRequest.post(
-        apiEndPoints.POST_RESET_PASSWORD,
+        apiEndPoints.POST_FORGOT_PASSWORD,
         data
       );
       return response?.data;
     },
     onSuccess: (data) => {
       if (data.msg) {
-        router.push('/auth/change-password?s=1', { replace: true });
-        SuccessToast({ text: data?.msg });
         cb();
-      } else {
-        router.push('/auth/change-password?s=0', { replace: true });
+        SuccessToast({ text: data?.msg });
+        setTimeout(() => {
+          router.push('/auth/login', { replace: true });
+        }, 5000);
       }
     },
     onError: (error) => {
--- src/utils/usePostChangePassword.js
+++ src/utils/usePostChangePassword.js
@@ -1,26 +1,21 @@
-import { useRouter } from 'next/navigation';
 import { useMutation } from '@tanstack/react-query';
 import apiEndPoints from '@/services/apiEndPoints';
 import makeApiRequest from '@/services/makeApiRequest';
 import { ErrorToast, SuccessToast } from '@/services/toasterServices';
 
-export default function usePostResetPassword(cb) {
-  const router = useRouter();
+export default function usePostChangePassword(cb) {
   return useMutation({
     mutationFn: async (data) => {
       const response = await makeApiRequest.post(
-        apiEndPoints.POST_RESET_PASSWORD,
+        apiEndPoints.POST_CHANGE_PASSWORD,
         data
       );
       return response?.data;
     },
     onSuccess: (data) => {
       if (data.msg) {
-        router.push('/auth/change-password?s=1', { replace: true });
-        SuccessToast({ text: data?.msg });
         cb();
-      } else {
-        router.push('/auth/change-password?s=0', { replace: true });
+        SuccessToast({ text: data?.msg });
       }
     },
     onError: (error) => {
