c073a50e4cc6b03a00c16966b7c3e6d808bf2c03
Kishore Mongar <84139548+fushigina-Shinobi@users.noreply.github.com>
2024-07-29 23:36:08 +0530

    feat: fallback for pasal items viewing added

--- src/app/(application)/(pasal)/books/page.jsx
+++ src/app/(application)/(pasal)/books/page.jsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useSearchParams } from 'next/navigation';
+import PasalFallback from '../_components/pasalFallback';
 import useGetAllProducts from '@/app/home/_hooks/getAllProducts';
 import PageLoadingOverlay from '@/components/common/pageLoadingOverlay';
 import ProductCards from '@/components/common/productCards';
@@ -10,20 +11,24 @@ export default function BooksPage() {
   const searchParams = useSearchParams();
   const queryParams = filterParams('books', searchParams);
   const { data: getAllApparel, isLoading } = useGetAllProducts(queryParams);
+  const checkData =
+    getAllApparel?.products?.length > 0 ? (
+      getAllApparel?.products?.map((product) => (
+        <ProductCards key={product?.id} product={product} pasal='books' />
+      ))
+    ) : (
+      <PasalFallback />
+    );
 
   return (
     <div
       className={`${
-        !isLoading && 'grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4'
+        !isLoading &&
+        getAllApparel?.products?.length > 0 &&
+        'grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4'
       }`}
     >
-      {isLoading ? (
-        <PageLoadingOverlay />
-      ) : (
-        getAllApparel?.products?.map((product) => (
-          <ProductCards key={product?.id} product={product} pasal='books' />
-        ))
-      )}
+      {isLoading ? <PageLoadingOverlay /> : checkData}
     </div>
   );
 }
--- src/app/(application)/(pasal)/krafts/page.jsx
+++ src/app/(application)/(pasal)/krafts/page.jsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useSearchParams } from 'next/navigation';
+import PasalFallback from '../_components/pasalFallback';
 import useGetAllProducts from '@/app/home/_hooks/getAllProducts';
 import PageLoadingOverlay from '@/components/common/pageLoadingOverlay';
 import ProductCards from '@/components/common/productCards';
@@ -10,20 +11,23 @@ export default function KraftsPage() {
   const searchParams = useSearchParams();
   const queryParams = filterParams('krafts', searchParams);
   const { data: getAllApparel, isLoading } = useGetAllProducts(queryParams);
-
+  const checkData =
+    getAllApparel?.products?.length > 0 ? (
+      getAllApparel?.products?.map((product) => (
+        <ProductCards key={product?.id} product={product} pasal='krafts' />
+      ))
+    ) : (
+      <PasalFallback />
+    );
   return (
     <div
       className={`${
-        !isLoading && 'grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4'
+        !isLoading &&
+        getAllApparel?.products?.length > 0 &&
+        'grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4'
       }`}
     >
-      {isLoading ? (
-        <PageLoadingOverlay />
-      ) : (
-        getAllApparel?.products?.map((product) => (
-          <ProductCards key={product?.id} product={product} pasal='krafts' />
-        ))
-      )}
+      {isLoading ? <PageLoadingOverlay /> : checkData}
     </div>
   );
 }
